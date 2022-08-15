b0ea62fa22a8699fc11e84e30a63d19398d2b2f4
Simen A. W. Olsen <so@bjerk.io>
2022-08-15 09:51:40 +0200

    wip

--- src/methods/fastify-plugin.ts
+++ src/methods/fastify-plugin.ts
@@ -1,44 +1,39 @@
-import fp from 'fastify-plugin';
+import { FastifyPluginAsync, FastifyRequest } from 'fastify';
 import { handlePubSubMessage } from '../common';
 import { PubSubConfig, PubSubRequest, PubSubRequestType } from '../types';
 
-export const pubSubFastifyPlugin = fp<PubSubConfig<any, any>>(
-  (fastify, options) => {
-    const { path = '/', handler, parseJson, onError } = options;
-
-    fastify.post<{
-      Body: PubSubRequestType;
-    }>(
-      path,
-      {
-        schema: {
-          body: PubSubRequest,
-          response: { 204: {} },
-        },
+export const pubSubFastifyPlugin: FastifyPluginAsync<
+  PubSubConfig<any, any>
+> = async (fastify, options) => {
+  const { path = '/', handler, parseJson, onError } = options;
+  fastify.post<{
+    Body: PubSubRequestType;
+  }>(
+    path,
+    {
+      schema: {
+        body: PubSubRequest,
+        response: { 204: {} },
       },
-      async (req, reply) => {
-        try {
-          const res = await handlePubSubMessage({
-            message: req.body.message,
-            handler,
-            parseJson,
-            // context: req,
-            log: req.log,
-          });
+    },
+    async (req, reply) => {
+      try {
+        const res = await handlePubSubMessage<FastifyRequest>({
+          message: req.body.message,
+          handler,
+          parseJson,
+          context: req,
+          log: req.log,
+        });
 
-          reply.code(res?.statusCode || 204);
-        } catch (error) {
-          if (onError) {
-            await onError(error);
-            reply.code(204);
-          } else throw error;
-        }
-        reply.send();
-      },
-    );
-  },
-  {
-    fastify: '4.x',
-    name: 'pubsub-http-handler',
-  },
-);
+        reply.code(res?.statusCode || 204);
+      } catch (error) {
+        if (onError) {
+          await onError(error);
+          reply.code(204);
+        } else throw error;
+      }
+      reply.send();
+    },
+  );
+};
