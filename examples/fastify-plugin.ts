b0347798f023da051bb9acf22aec71397fbf621c
Simen A. W. Olsen <so@bjerk.io>
2022-08-27 22:27:54 +0200

    update example with fn

--- examples/fastify-plugin.ts
+++ examples/fastify-plugin.ts
@@ -1,7 +1,7 @@
 import { TypeBoxTypeProvider } from '@fastify/type-provider-typebox';
 import Fastify from 'fastify';
 import { z } from 'zod';
-import { pubSubFastifyPlugin } from '../src';
+import { makePubSubConfig, pubSubFastifyPlugin } from '../src';
 
 const inputData = z.object({
   name: z.string(),
@@ -14,13 +14,18 @@ const inputData = z.object({
 const server = () => {
   const fastify = Fastify().withTypeProvider<TypeBoxTypeProvider>();
 
-  fastify.register(pubSubFastifyPlugin, {
-    parser: d => inputData.parse(d),
-    handler: ({ data, log }) => {
-      const { name, party, bookingId } = data;
-      log.info(`${name} from ${party.name} had a booking with id ${bookingId}`);
-    },
-  });
+  fastify.register(
+    pubSubFastifyPlugin,
+    makePubSubConfig({
+      parser: d => inputData.parse(d),
+      handler: ({ data, log }) => {
+        const { name, party, bookingId } = data;
+        log.info(
+          `${name} from ${party.name} had a booking with id ${bookingId}`,
+        );
+      },
+    }),
+  );
 
   fastify.server.listen(8000);
 };
