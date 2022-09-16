2898343fd98ae2ef9f27cc112aef5cb0be396254
Simen Bekkhus <sbekkhus91@gmail.com>
2022-09-16 15:01:54 +0200

    feat: always provide `context` argument
    Also provide it to `onError`

--- src/methods/cloud-functions.ts
+++ src/methods/cloud-functions.ts
@@ -20,11 +20,12 @@ export function createPubSubCloudFunctions<Data = unknown, Context = unknown>(
 ): CloudFunctionFun {
   const { parseJson, onError, logger } = options;
   return async (req, res): Promise<void> => {
+    const context: Context = req.body;
     try {
       await handlePubSubMessage({
         message: req.body.message,
         handler,
-        context: req.body,
+        context,
         parseJson,
         log: pino(gcpLogOptions(logger)),
       });
@@ -32,9 +33,11 @@ export function createPubSubCloudFunctions<Data = unknown, Context = unknown>(
       res.status(200).send();
     } catch (error) {
       if (onError) {
-        await onError(error);
+        await onError(error, context);
         res.status(200).send();
-      } else throw error;
+      } else {
+        throw error;
+      }
     }
   };
 }
